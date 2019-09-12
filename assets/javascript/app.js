d4d5e657bccda0dce80e8a385e4e3a6f0534a1a2
Mahi Mani <m.mahishamani@gmail.com>
2019-09-11 22:30:17 -0700

    added styles to stylesheet

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,6 +6,7 @@ var i;
 // var queryURL = "https://api.giphy.com/v1/gifs/" + xT4uQulxzV39haRFjG + "?api_key=fwPvz04E3buxTZ95FnxM8IJwGBRPT7dZ";
 
 function searchGif(){
+    $("#allDiv").show();
     var animal = $(this).attr("data-name");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=fwPvz04E3buxTZ95FnxM8IJwGBRPT7dZ&limit=10";
 
@@ -18,39 +19,76 @@ function searchGif(){
         for(var i=0;i<image.length;i++){
         // $("#images").append("<br><br><img src="+image[i].images.fixed_height.url+">");
         var imageDiv = $("<div>");
-        // var pDiv = $("<div>");
+        // var pDiv = $("<div>"); 
         var imageTag = $("<img>");
         imageTag.attr("src",image[i].images.downsized_still.url);
         imageTag.attr("width","150px");
         imageTag.attr("height","150px");
         imageTag.addClass("animalGif");
-        imageTag.addClass("inline");
+        imageTag.addClass("column");
         imageTag.attr("data-still",image[i].images.downsized_still.url);
         imageTag.attr("data-animate",image[i].images.fixed_height.url);
         imageTag.attr("data-state","still");
         var rating = image[i].rating;
-        // var p = $("<p>").text("Rating: " + rating);
+        var title = image[i].title.trim("GIF");
+        var p = $("<p>").text("Rating: " + rating);
+        p.addClass("column");
         console.log("Rating: "+rating);
-        // $("#gifView").append(rating);
-        // $("#images").append(imageTag);
-        // imageDiv.html(imageTag);
-        // pDiv.append(p);
-        // imageDiv.text(imageTag);
-        // pDiv.text(p);
-        // $("#allDiv").prepend(imageDiv);
-        
-
-        // $('#container').append("<p>It worked chaps</p>");
         imageDiv.append(imageTag);
         imageDiv.prepend("<p>Rating : " + rating + "</p>");
-        console.log("image tag: "+JSON.stringify(imageTag));
-        $("#allDiv").prepend(imageTag);
+        imageDiv.prepend("<p>Title : "+ title + "</p>");
         
+        $(".row").prepend(imageDiv);
+        $(".row").prepend("<br><hr>");
         }
+});
+
+}
+
+function randomGif(event){
+  event.preventDefault();
+  // var animal = $(this).attr("data-name");
+  var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=fwPvz04E3buxTZ95FnxM8IJwGBRPT7dZ&limit=10&random_id=e826c9fc5c929e0d6c6d423841a282aa";
+
+  $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+      console.log(response);
+      var gifs = response.data;
+      // console.log("gifs"+gifs);
+      // $("#images").append("<br><br><img src="+image[i].images.fixed_height.url+">");
+      var newimageDiv = $("<div>");
+      // var pDiv = $("<div>"); 
+      var newimageTag = $("<img>");
+      newimageTag.attr("src",gifs.images.downsized_still.url);
+      console.log("image src "+gifs.images.downsized_still.url);
+      newimageTag.attr("width","150px");
+      newimageTag.attr("height","150px");
+      newimageTag.addClass("animalGif");
+      newimageTag.addClass("column");
+      newimageTag.attr("data-still",gifs.images.downsized_still.url);
+      newimageTag.attr("data-animate",gifs.images.fixed_height.url);
+      newimageTag.attr("data-state","still");
+      // var rating = gifs.rating;
+      // var newp = $("<p>").text("");
+      // newp.addClass("column");
+      // console.log("Rating: "+rating);
+      var title = gifs.title;
+      newimageDiv.append(newimageTag);
+      newimageDiv.prepend("<p>"+ title + "</p>");
+      // newimageDiv.prepend("<p>Rating : " + rating + "</p>");
+      $("#allDiv").show();
+      $(".row").prepend(newimageDiv);
+
+      $(".row").prepend("<br>");
+    
 
 
 });
+
 }
+ 
         // To pause gif image when clicked
         $(document).on("click",".animalGif",function(){
           var state = $(this).attr("data-state");
@@ -98,5 +136,17 @@ function searchGif(){
         generateButton();
       });
 
+      $("#clear").on("click",function(event){
+        event.preventDefault();
+        $("#allDiv").hide();
+        $(".row").empty();
+        $("#enterAnimals").val("");
+
+      });
+
+
       $(document).on("click",".animalClass",searchGif);
+      $("#addGif").on("click", randomGif);
+
+      
       generateButton();
